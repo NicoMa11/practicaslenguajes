87cafb217e4a52a3bde68ca04fb20b7de82a425a
NicoMa11 <90228841+NicoMa11@users.noreply.github.com>
2023-09-16 09:55:13 -0800

    Add files via upload

---
+++ Practica Funcional/ejercicio 2.2.fs
@@ -0,0 +1,10 @@
+﻿// ejercicio 2.2
+open System
+
+let filtrarPorSubcadena (pal:string) (lista:string list) =
+        lista
+        |> List.filter (fun str -> System.Text.RegularExpressions.Regex(sprintf "\\b%s\\b"  pal).IsMatch(str))
+let lista = ["perro bravo"; "la casa"; "el perro"; "pintando la cerca"; "mi mascota es un perro"]
+let subcadena = "perro"
+let resultado = filtrarPorSubcadena subcadena lista
+printfn "%A" resultado
---
+++ Practica Funcional/ejercicio 2.3.fs
@@ -0,0 +1,20 @@
+﻿// ejercicio 2.3
+
+let n_esimo n lista =
+    let indices = [0..(List.length lista - 1)]
+    let elementos = List.map (fun i -> if i = n then Some (List.nth lista i) else None) indices
+    match List.choose id elementos with
+    | [resultado] -> Some resultado
+    | _ -> None
+    
+//Salidas
+let lista1 = [1; 2; 3; 4; 5]
+printfn "Lista original: %A" lista1
+let resultado1 = n_esimo 2 lista1
+printfn "n_esimo 2: %A" resultado1 
+
+let resultado2 = n_esimo 3 lista1
+printfn "n_esimo 3: %A" resultado2  
+
+let resultado3 = n_esimo 6 lista1
+printfn "n_esimo 6: %A" resultado3  
